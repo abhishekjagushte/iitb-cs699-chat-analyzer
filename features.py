bc22163d20745555c7510341ac1679a88ad34607
Udhay Brahmi <72250606+Udhay-Brahmi@users.noreply.github.com>
2023-11-06 10:47:00 +0530

    Update features.py

--- features.py
+++ features.py
@@ -1,12 +1,19 @@
 import pandas as pd
 
-# -1 => Negative
+#-1 => Negative
 # 0 => Neutral
 # 1 => Positive
 
-#Count of messages of selected user per day having k(0/1/-1) sentiment.
-def weekly(selected_user,df,k):
-    if selected_user != 'Overall':
-        df = df[df['user'] == selected_user]
-    df = df[df['value'] == k]
+#Count of messages of selected user per day having (0, 1, -1) sentiment.
+def weekly(selected,df,sentiment):
+    if selected != 'Overall':
+        df = df[df['user'] == selected]
+    df = df[df['value'] == sentiment]
     return df['day_name'].value_counts()
+
+#Count of messages of selected user per month having (0, 1, -1) sentiment.
+def monthly(selected,df,sentiment):
+    if selected != 'Overall':
+        df = df[df['user'] == selected]
+    df = df[df['value'] == sentiment]
+    return df['month'].value_counts()
