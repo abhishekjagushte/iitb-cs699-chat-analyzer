16b3892c7921590cd5705edba38ee4de5453eaaf
Udhay Brahmi <72250606+Udhay-Brahmi@users.noreply.github.com>
2023-11-10 10:27:11 +0530

    Word cloud added.

--- features.py
+++ features.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from wordcloud import WordCloud
 
 #-1 => Negative
 # 0 => Neutral
@@ -43,3 +44,23 @@ def percentage(df,sentiment):
     df = round((df['user'][df['value']==sentiment].value_counts() / df[df['value']==sentiment].shape[0]) * 100, 2).reset_index().rename(
         columns={'index': 'name', 'user': 'percent'})
     return df
+
+# Wordcloud from words in message.
+def wordcloud(selected,df,sentiment):
+    f = open('stop_hinglish.txt', 'r')
+    stop_words = f.read()
+    if selected != 'Overall':
+        df = df[df['user'] == selected]
+    temp = df[df['user'] != 'group_notification']
+    temp = temp[temp['message'] != '<Media omitted>\n']
+    def remove_stop_words(message):
+        y = []
+        for word in message.lower().split():
+            if word not in stop_words:
+                y.append(word)
+        return " ".join(y)
+    wc = WordCloud(width=500,height=500,min_font_size=10,background_color='white')
+    temp['message'] = temp['message'].apply(remove_stop_words)
+    temp['message'] = temp['message'][temp['value'] == sentiment]
+    df_wc = wc.generate(temp['message'].str.cat(sep=" "))
+    return df_wc
