f20ca47f2bf37d97f2815c81c6bb366bc68cc3f0
Udhay Brahmi <72250606+Udhay-Brahmi@users.noreply.github.com>
2023-11-14 11:36:38 +0530

    Update analysis_result.py

--- analysis_result.py
+++ analysis_result.py
@@ -8,7 +8,6 @@ def create_monthly_analysis(selected_user, data):
     with col1:
         st.markdown("### Positive")
         busy_month = features.monthly(selected_user, data,1)
-        
         fig, ax = plt.subplots()
         ax.bar(busy_month.index, busy_month.values, color='green')
         plt.xticks(rotation='vertical')
@@ -24,10 +23,33 @@ def create_monthly_analysis(selected_user, data):
 
     with col3:
         st.markdown("### Neutral")
-       
         busy_month = features.monthly(selected_user, data, 0)
-        
         fig, ax = plt.subplots()
-        ax.bar(busy_month.index, busy_month.values, color='grey')
+        ax.bar(busy_month.index, busy_month.values, color='red')
+        plt.xticks(rotation='vertical')
+        st.pyplot(fig)
+        
+def create_weekly_analysis(selected, data):
+    st.markdown("## Weekly analysis")
+    col1, col2, col3 = st.columns(3)
+    with col1:
+        st.markdown("### Positive")
+        busy_day = features.weekly(selected, data,1)
+        fig, ax = plt.subplots()
+        ax.bar(busy_day.index, busy_day.values, color='green')
+        plt.xticks(rotation='vertical')
+        st.pyplot(fig)
+    with col2:
+        st.markdown("### Neutral")
+        busy_day = features.weekly(selected, data, 0)
+        fig, ax = plt.subplots()
+        ax.bar(busy_day.index, busy_day.values, color='grey')
+        plt.xticks(rotation='vertical')
+        st.pyplot(fig)
+    with col3:
+        st.markdown("### Negative")
+        busy_day = features.weekly(selected, data, -1)
+        fig, ax = plt.subplots()
+        ax.bar(busy_day.index, busy_day.values, color='red')
         plt.xticks(rotation='vertical')
         st.pyplot(fig)
